4e2e1bc5151d2637fac60d046aa9a8ea089e02d4
qwerti <59662685+QWERTYONDRA@users.noreply.github.com>
2023-01-25 21:13:39 +0100

    patch e
    e for error coz i flipped up

--- main/animations.js
+++ main/animations.js
@@ -10,3 +10,12 @@ function expandList() {
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("email").innerText="ondrasihonda@gmail.com";    
 });
+const element = document.getElementById("my-element");
+
+element.addEventListener("mouseover", function() {
+  element.style.color = "gray";
+});
+
+element.addEventListener("mouseout", function() {
+  element.style.color = "white";
+});
